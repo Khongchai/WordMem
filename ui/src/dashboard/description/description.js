e66f60e05b84ee54343601335dcd8f68e48df21b
Khongchai <world1955@hotmail.com>
2020-11-05 15:43:49 +0700

    fixed description position

--- ui/src/dashboard/description/description.js
+++ ui/src/dashboard/description/description.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function(props)
 {
     return(
-        <div style={{padding: "3rem", margin: "30px"}}>
+        <div className="description">
             {props.meaning}
         </div>
     )
