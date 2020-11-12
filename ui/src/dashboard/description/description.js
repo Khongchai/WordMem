fb3a1966a2bc9cf03b0f238a5afae45d081143ad
Khongchai <world1955@hotmail.com>
2020-11-13 04:55:33 +0700

    added "synonyms" indication

--- ui/src/dashboard/description/description.js
+++ ui/src/dashboard/description/description.js
@@ -10,6 +10,9 @@ export default function(props)
             </div> 
             <div className="synonym-list">
                 <ui>
+                    {synonymList.length > 0?
+                     <div style={{fontStyle: "normal", fontWeight: "bold", marginBottom: "1rem"}}>Synonyms: </div>
+                      : ""}
                     {synonymList.map(syn => (
                         <li id={syn}>
                             {syn}
